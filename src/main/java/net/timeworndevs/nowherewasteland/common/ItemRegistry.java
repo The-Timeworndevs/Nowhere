231b7a863c54344e90632d540f21b220912b4106
{VoxelDragon6} <58091259+HexBugOrion@users.noreply.github.com>
2024-11-17 21:03:12 -0600

    Whoops
    Wrong neoforge mdk lol

---
+++ src/main/java/net/timeworndevs/nowherewasteland/common/ItemRegistry.java
@@ -0,0 +1,10 @@
+package net.timeworndevs.nowherewasteland.common;
+
+import net.neoforged.neoforge.registries.DeferredRegister;
+import net.timeworndevs.nowherewasteland.Main;
+
+public class ItemRegistry {
+
+    public static final DeferredRegister.Items ITEM_REGISTRIES = DeferredRegister.createItems(Main.MODID);
+
+}
