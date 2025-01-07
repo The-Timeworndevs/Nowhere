acdbee655b22b5710697f43a63a4244816ed9592
{VoxelDragon6} <58091259+HexBugOrion@users.noreply.github.com>
2025-01-07 12:33:29 -0600

    plancc

--- src/main/java/net/timeworndevs/nowhere/common/BlockRegistry.java
+++ src/main/java/net/timeworndevs/nowhere/common/BlockRegistry.java
@@ -24,6 +24,7 @@ public class BlockRegistry {
     //Blocks
     public static final DeferredBlock<Block> PARCHED_LOG = BLOCK_REGISTRIES.register("parched_log", () -> new RotatedPillarBlock(BlockBehaviour.Properties.of().mapColor(MapColor.TERRACOTTA_ORANGE).sound(SoundType.STEM).strength(2.5F, 2.5F)));
     public static final DeferredBlock<Block> STRIPPED_PARCHED_LOG = BLOCK_REGISTRIES.register("stripped_parched_log", () -> new RotatedPillarBlock(BlockBehaviour.Properties.of().mapColor(MapColor.TERRACOTTA_ORANGE).sound(SoundType.STEM).strength(2.5F, 2.5F)));
+    public static final DeferredBlock<Block> PARCHED_PLANKS = BLOCK_REGISTRIES.registerSimpleBlock("parched_planks", BlockBehaviour.Properties.of().mapColor(MapColor.TERRACOTTA_ORANGE).sound(SoundType.NETHER_WOOD).strength(2.5F, 2.5F));
     public static final DeferredBlock<Block> DUST_BLOCK = BLOCK_REGISTRIES.register("dust_block", () -> new FallingBlock(BlockBehaviour.Properties.of().mapColor(MapColor.SAND).sound(SoundType.SOUL_SAND).strength(0.5F, 0.5F)) {
         @Override
         protected MapCodec<? extends FallingBlock> codec() {
@@ -36,13 +37,14 @@ public class BlockRegistry {
     public static final DeferredBlock<Block> DUSTSTONE_BRICKS = BLOCK_REGISTRIES.registerSimpleBlock("duststone_bricks", BlockBehaviour.Properties.of().mapColor(MapColor.SAND).sound(SoundType.DRIPSTONE_BLOCK).strength(0.8F,0.8F));
     public static final DeferredBlock<Block> FERROUS_ALLOY_BLOCK = BLOCK_REGISTRIES.registerSimpleBlock("ferrous_alloy_block", BlockBehaviour.Properties.of().mapColor(MapColor.TERRACOTTA_LIGHT_BLUE).sound(SoundType.HEAVY_CORE).strength(5F,6F));
     public static final DeferredBlock<Block> SALT_CRUST = BLOCK_REGISTRIES.registerSimpleBlock("salt_crust", BlockBehaviour.Properties.of().mapColor(MapColor.SAND).sound(SoundType.CALCITE).strength(0.5F,0.5F));
-    public static final DeferredBlock<Block> BITUMEN_BLOCK = BLOCK_REGISTRIES.register("bitumen_block", () -> new SoulSandBlock(BlockBehaviour.Properties.of().mapColor(MapColor.COLOR_BLACK).sound(SoundType.MUD_BRICKS).strength(0.5F,0.5F).speedFactor(0.4F)));
+    public static final DeferredBlock<Block> BITUMEN_BLOCK = BLOCK_REGISTRIES.register("bitumen_block", () -> new SoulSandBlock(BlockBehaviour.Properties.of().mapColor(MapColor.COLOR_BLACK).sound(SoundType.HONEY_BLOCK).strength(0.5F,0.5F).speedFactor(0.4F)));
     public static final DeferredBlock<Block> MAKESHIFT_TORCH = BLOCK_REGISTRIES.register("makeshift_torch", () -> new TorchBlock(ParticleTypes.FLAME, BlockBehaviour.Properties.of().mapColor(MapColor.FIRE).sound(SoundType.NETHER_WOOD).instabreak().pushReaction(PushReaction.DESTROY).lightLevel((level) -> {return 12;})));
     public static final DeferredBlock<Block> WALL_MAKESHIFT_TORCH = BLOCK_REGISTRIES.register("makeshift_wall_torch", () -> new WallTorchBlock(ParticleTypes.FLAME, BlockBehaviour.Properties.of().mapColor(MapColor.FIRE).sound(SoundType.NETHER_WOOD).instabreak().pushReaction(PushReaction.DESTROY).lightLevel((level) -> {return 12;})));
 
     //BlockItems
     public static final DeferredItem<BlockItem> PARCHED_LOG_BLOCK_ITEM = ItemRegistry.ITEM_REGISTRIES.registerSimpleBlockItem("parched_log", PARCHED_LOG);
     public static final DeferredItem<BlockItem> STRIPPED_PARCHED_LOG_BLOCK_ITEM = ItemRegistry.ITEM_REGISTRIES.registerSimpleBlockItem("stripped_parched_log", STRIPPED_PARCHED_LOG);
+    public static final DeferredItem<BlockItem> PARCHED_PLANKS_BLOCK_ITEM = ItemRegistry.ITEM_REGISTRIES.registerSimpleBlockItem("parched_planks", PARCHED_PLANKS);
     public static final DeferredItem<BlockItem> DUST_BLOCK_ITEM = ItemRegistry.ITEM_REGISTRIES.registerSimpleBlockItem("dust_block", DUST_BLOCK);
     public static final DeferredItem<BlockItem> DUSTY_BITUMEN_BLOCK_ITEM = ItemRegistry.ITEM_REGISTRIES.registerSimpleBlockItem("dusty_bitumen", DUSTY_BITUMEN);
     public static final DeferredItem<BlockItem> PACKED_DUST_BLOCK_ITEM = ItemRegistry.ITEM_REGISTRIES.registerSimpleBlockItem("packed_dust_block", PACKED_DUST_BLOCK);
--- src/main/java/net/timeworndevs/nowhere/common/GroupRegistry.java
+++ src/main/java/net/timeworndevs/nowhere/common/GroupRegistry.java
@@ -14,19 +14,44 @@ public class GroupRegistry {
 
     public static final DeferredHolder<CreativeModeTab, CreativeModeTab> NOWHERE_BLOCKS_TAB = GROUP_REGISTRIES.register("nowhere_blocks", () -> CreativeModeTab.builder()
             .title(Component.translatable("itemGroup.nowhere.blocks"))
-            .icon(() -> BlockRegistry.DUST_BLOCK_ITEM.get().getDefaultInstance())
+            .icon(() -> BlockRegistry.DUSTSTONE_BRICKS_BLOCK_ITEM.get().getDefaultInstance())
             .displayItems((parameters, output) -> {
                 output.accept(BlockRegistry.PARCHED_LOG_BLOCK_ITEM.get());
                 output.accept(BlockRegistry.STRIPPED_PARCHED_LOG_BLOCK_ITEM.get());
-                output.accept(BlockRegistry.DUST_BLOCK_ITEM.get());
-                output.accept(BlockRegistry.DUSTY_BITUMEN_BLOCK_ITEM.get());
-                output.accept(BlockRegistry.PACKED_DUST_BLOCK_ITEM.get());
+                output.accept(BlockRegistry.PARCHED_PLANKS_BLOCK_ITEM.get());
                 output.accept(BlockRegistry.DUSTSTONE_BLOCK_ITEM.get());
                 output.accept(BlockRegistry.DUSTSTONE_BRICKS_BLOCK_ITEM.get());
                 output.accept(BlockRegistry.FERROUS_ALLOY_BLOCK_ITEM.get());
-                output.accept(BlockRegistry.SALT_CRUST_BLOCK_ITEM.get());
                 output.accept(BlockRegistry.BITUMEN_BLOCK_ITEM.get());
+            }).build());
+
+    public static final DeferredHolder<CreativeModeTab, CreativeModeTab> NOWHERE_NATURAL_TAB = GROUP_REGISTRIES.register("nowhere_natural_blocks", () -> CreativeModeTab.builder()
+            .title(Component.translatable("itemGroup.nowhere.natural"))
+            .icon(() -> BlockRegistry.DUST_BLOCK_ITEM.get().getDefaultInstance())
+            .displayItems((parameters, output) -> {
+                output.accept(BlockRegistry.DUST_BLOCK_ITEM.get());
+                output.accept(BlockRegistry.DUSTY_BITUMEN_BLOCK_ITEM.get());
+                output.accept(BlockRegistry.PACKED_DUST_BLOCK_ITEM.get());
+                output.accept(BlockRegistry.SALT_CRUST_BLOCK_ITEM.get());
+            }).build());
+
+    public static final DeferredHolder<CreativeModeTab, CreativeModeTab> NOWHERE_FUNCTIONAL_TAB = GROUP_REGISTRIES.register("nowhere_functional_blocks", () -> CreativeModeTab.builder()
+            .title(Component.translatable("itemGroup.nowhere.functional"))
+            .icon(() -> BlockRegistry.MAKESHIFT_TORCH_BLOCK_ITEM.get().getDefaultInstance())
+            .displayItems((parameters, output) -> {
                 output.accept(BlockRegistry.MAKESHIFT_TORCH_BLOCK_ITEM.get());
+            }).build());
+
+    public static final DeferredHolder<CreativeModeTab, CreativeModeTab> NOWHERE_TOOLS_TAB = GROUP_REGISTRIES.register("nowhere_tools", () -> CreativeModeTab.builder()
+            .title(Component.translatable("itemGroup.nowhere.tools"))
+            .icon(() -> ItemRegistry.FERROUS_ALLOY_NUGGET.get().getDefaultInstance())
+            .displayItems((parameters, output) -> {
+            }).build());
+
+    public static final DeferredHolder<CreativeModeTab, CreativeModeTab> NOWHERE_COMBAT_TAB = GROUP_REGISTRIES.register("nowhere_combat", () -> CreativeModeTab.builder()
+            .title(Component.translatable("itemGroup.nowhere.combat"))
+            .icon(() -> ItemRegistry.FERROUS_SCRAP.get().getDefaultInstance())
+            .displayItems((parameters, output) -> {
             }).build());
 
     public static final DeferredHolder<CreativeModeTab, CreativeModeTab> NOWHERE_MATERIALS_TAB = GROUP_REGISTRIES.register("nowhere_materials", () -> CreativeModeTab.builder()
