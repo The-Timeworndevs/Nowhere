02765ca1f42726fbe311f41d9c387c289cfd72db
{VoxelDragon6} <58091259+HexBugOrion@users.noreply.github.com>
2026-06-18 20:30:17 -0500

    Fixed crash upon placing a sign
    New issue: The hanging signs have offset UVs that I do not know how to fix

--- src/main/java/net/tws/nowhere/common/blocks/NWWoodTypes.java
+++ src/main/java/net/tws/nowhere/common/blocks/NWWoodTypes.java
@@ -5,6 +5,5 @@ import net.minecraft.world.level.block.state.properties.WoodType;
 
 public class NWWoodTypes {
 
-    public static final WoodType PARCHED = new WoodType("parched", NWBlockSetTypes.PARCHED);
-
+    public static final WoodType PARCHED = WoodType.register(new WoodType("parched", NWBlockSetTypes.PARCHED));
 }
