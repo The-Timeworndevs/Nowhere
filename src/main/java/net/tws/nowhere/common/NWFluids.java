d664993a95dcebc044071f6172495907a20bf0e2
{VoxelDragon6} <58091259+HexBugOrion@users.noreply.github.com>
2026-07-03 11:23:47 -0500

    Fixed typo in NWFluids

--- src/main/java/net/tws/nowhere/common/NWFluids.java
+++ src/main/java/net/tws/nowhere/common/NWFluids.java
@@ -13,7 +13,7 @@ public class NWFluids {
     public static final DeferredRegister<Fluid> FLUIDS = DeferredRegister.create(Registries.FLUID, Nowhere.MODID);
 
     public static final DeferredHolder<Fluid, BaseFlowingFluid.Source> SOURCE_ULTRASALINE_WATER = FLUIDS.register("source_ultrasaline_water", ()-> new BaseFlowingFluid.Source(NWFluids.ULTRASALINE_WATER_PROPERTIES));
-    public static final DeferredHolder<Fluid, BaseFlowingFluid.Flowing> FLOWING_ULTRASALINE_WATER = FLUIDS.register("sflowing_ultrasaline_water", ()-> new BaseFlowingFluid.Flowing(NWFluids.ULTRASALINE_WATER_PROPERTIES));
+    public static final DeferredHolder<Fluid, BaseFlowingFluid.Flowing> FLOWING_ULTRASALINE_WATER = FLUIDS.register("flowing_ultrasaline_water", ()-> new BaseFlowingFluid.Flowing(NWFluids.ULTRASALINE_WATER_PROPERTIES));
 
     public static final BaseFlowingFluid.Properties ULTRASALINE_WATER_PROPERTIES = new BaseFlowingFluid.Properties(NWFluidTypes.ULTRASALINE_WATER_TYPE, SOURCE_ULTRASALINE_WATER, FLOWING_ULTRASALINE_WATER).slopeFindDistance(15).levelDecreasePerBlock(1).block(NWBlocks.ULTRASALINE_WATER);
 
